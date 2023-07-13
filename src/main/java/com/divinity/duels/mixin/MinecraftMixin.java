2d79c6d3d807c4d145ee4523c45dbebe23207e3a
Divinity <69832329+Divine144@users.noreply.github.com>
2023-07-13 16:53:35 -0400

    [publish] fix

--- src/main/java/com/divinity/duels/mixin/MinecraftMixin.java
+++ src/main/java/com/divinity/duels/mixin/MinecraftMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Minecraft.class)
@@ -19,11 +20,26 @@ public class MinecraftMixin {
 
     @Shadow private volatile boolean pause;
 
-    @Inject(method= "runTick", at=@At("TAIL"))
+    @Redirect(method= "runTick", at=@At(value = "INVOKE", target = "Lnet/minecraft/client/Timer;advanceTime(J)I"))
+    public int advanceTime(Timer instance, long pGameTime) {
+        var instanceM = (Minecraft) (Object) this;
+        if (instanceM.level != null) {
+            var cap = GlobalCapAttacher.getGlobalLevelCapabilityUnwrap(instanceM.level);
+            if (cap != null) {
+                if (cap.isEnabled()) {
+                    instance.advanceTime(pGameTime);
+                    return 1; // When timer.partialTick = pausePartialTick, sometimes this can be 0, which prevents the for loop from executing the tick method
+                }
+            }
+        }
+        return instance.advanceTime(pGameTime);
+    }
+
+    @Inject(method= "runTick", at=@At(value = "TAIL"))
     public void runTick(boolean pRenderLevel, CallbackInfo ci) {
-        var instance = (Minecraft) (Object) this;
-        if (instance.level != null) {
-            var cap = GlobalCapAttacher.getGlobalLevelCapabilityUnwrap(instance.level);
+        var instanceM = (Minecraft) (Object) this;
+        if (instanceM.level != null) {
+            var cap = GlobalCapAttacher.getGlobalLevelCapabilityUnwrap(instanceM.level);
             if (cap != null) {
                 if (cap.isEnabled()) {
                     this.timer.partialTick = this.pausePartialTick;
